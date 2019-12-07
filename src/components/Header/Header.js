890cb872f19029b54beed54d28ff2276d5e94f8a
NIkola Vujanovic <nikola.vujanovic.dzonson@gmail.com>
2019-12-07 20:07:56 +0100

    Za Milutina

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Header.css'
+import logo from '../../img/logo.svg'
+import { Search } from '../Search/Search';
 
 const Header = () =>
 (
-    <header>This is header</header>
+    <header>
+        <div className="wrapper">
+            <img className="logo" src={logo}></img>
+            <Search/>
+        </div>
+    </header>
 )
 
 export {Header};
---
+++ src/components/Search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Search.css';
+
+const Search = () =>
+{
+    return(
+        <input className="search" type="text"/>
+    );
+}
+
+export {Search};
